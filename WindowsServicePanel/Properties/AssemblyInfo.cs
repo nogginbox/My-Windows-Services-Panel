5ef051df7024333c7af565726440b8cd223de4c2
Richard Garside <richard.garside@nogginbox.co.uk>
2018-10-25 08:04:43 +0100

    Updated installer and version numbers for v1.3

--- WindowsServicePanel/Properties/AssemblyInfo.cs
+++ WindowsServicePanel/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("My Windows Service Panel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Lets you quickly start and stop the Windows Services you care about.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Garsonix")]
 [assembly: AssemblyProduct("My Windows Service Panel")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
